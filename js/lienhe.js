e98906bd99302c9e105e5b6f8a68b446eec57a68
Quanh0810 <nqunhanh35@gmail.com>
2022-04-29 17:28:54 +0700

    final

---
+++ js/lienhe.js
@@ -0,0 +1,14 @@
+let inputReview = document.getElementById('Reivew');
+
+btnLogin.addEventListener('click', () => {
+    if (inputReview === "") {
+        alert("Bạn chưa nhập ý kiến");
+    }
+    else {
+        alert("Ý kiến của bạn đã được gửi");
+    }
+});
+
+
+
+
